253e3d3d64229fe63f66520404fdbe68c1ed86ce
Stevenodolf <stevenodolf@gmail.com>
2022-01-06 14:57:38 +0700

    Changes to web php

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Route::get('/', function () {
-//     return view('wish.createWish');
+//     return view('detailAkun.detailAkun');
 // });
 
+Route::get('/', 'HomeController@home');
 
 Route::get('/login', 'Auth\LoginController@getLogin')->name('login');
 Route::post('/login', 'Auth\LoginController@postLogin');
