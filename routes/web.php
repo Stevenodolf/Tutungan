47dd5584b2f2bfcb13c24244967267fb5c240ce4
Stevenodolf <stevenodolf@gmail.com>
2022-01-06 13:17:54 +0700

    Changes to web php

--- routes/web.php
+++ routes/web.php
@@ -13,19 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
- Route::get('/', function () {
-     return view('wish.createWish');
- });
+// Route::get('/', function () {
+//     return view('wish.createWish');
+// });
 
-//
-//Route::get('/login', 'Auth\LoginController@getLogin')->name('login');
-//Route::post('/login', 'Auth\LoginController@postLogin');
-//Route::get('/register', 'Auth\RegisterController@getRegister')->name('register');
-//Route::post('/register', 'Auth\RegisterController@postRegister');
-//
-//Route::get('/wish/{id}', 'WishController@wishDetail');
-//Route::post('/wish/{id}', 'CartController@addCart');
-//Route::get('/wish/delete-cart/{cart_id}', 'CartController@deleteCart');
-//Route::get('/cart', 'CartController@cart');
-//
-//Route::post('/checkout', 'TransactionController@checkOut');
+
+Route::get('/login', 'Auth\LoginController@getLogin')->name('login');
+Route::post('/login', 'Auth\LoginController@postLogin');
+Route::get('/register', 'Auth\RegisterController@getRegister')->name('register');
+Route::post('/register', 'Auth\RegisterController@postRegister');
+
+Route::get('/wish/{id}', 'WishController@wishDetail');
+Route::post('/wish/{id}', 'CartController@addCart');
+Route::get('/wish/delete-cart/{cart_id}', 'CartController@deleteCart');
+Route::get('/cart', 'CartController@cart');
+
+Route::post('/checkout', 'TransactionController@checkOut');
