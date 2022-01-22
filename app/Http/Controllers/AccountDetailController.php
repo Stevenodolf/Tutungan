f48061fca82a50493257b8badc81ae6414892466
Daniel Santoso <51028951+denaiels@users.noreply.github.com>
2022-01-22 20:06:48 +0700

    Added sorting from recently made
    Sorting in Wish Saya and Transaksi Saya

--- app/Http/Controllers/AccountDetailController.php
+++ app/Http/Controllers/AccountDetailController.php
@@ -31,13 +31,13 @@ class AccountDetailController extends Controller
             $user = User::where('id', Auth::user()->id)->first();
 
             if($request->filter == 0 or $request->filter == null) {
-                $wishes = Wish::where('created_by', $user->id)->get();
+                $wishes = Wish::where('created_by', $user->id)->orderBy('created_at', 'DESC')->get();
                 $filter = 0;
             }else{
                 $filter = $request->filter;
-                $wishes = Wish::where('created_by', $user->id)->where('status_wish_id', $filter)->get();
+                $wishes = Wish::where('created_by', $user->id)->where('status_wish_id', $filter)->orderBy('created_at', 'DESC')->get();
             }
-            $transactions = Transaction::where('user_id', $user->id)->get();
+            $transactions = Transaction::where('user_id', $user->id)->orderBy('created_at', 'DESC')->get();
 
             return view('detailAkun.wishSaya.wishSaya', ['user' => $user, 'wishes' => $wishes, 'filter' => $filter, 'transactions' => $transactions]);
         }
@@ -52,11 +52,11 @@ class AccountDetailController extends Controller
             $user = User::where('id', Auth::user()->id)->first();
 
             if($request->filter == 7 or $request->filter == null) {
-                $transactions = Transaction::where('user_id', $user->id)->where('status_transaksi_id', '!=', 0)->get();
+                $transactions = Transaction::where('user_id', $user->id)->where('status_transaksi_id', '!=', 0)->orderBy('created_at', 'DESC')->get();
                 $filter = 7;
             }else{
                 $filter = $request->filter;
-                $transactions = Transaction::where('user_id', $user->id)->where('status_transaksi_id', '!=', 0)->where('status_transaksi_id', $filter)->get();
+                $transactions = Transaction::where('user_id', $user->id)->where('status_transaksi_id', '!=', 0)->where('status_transaksi_id', $filter)->orderBy('created_at', 'DESC')->get();
             }
 
             return view('detailAkun.transaksiSaya.transaksiSaya', ['user' => $user, 'transactions' => $transactions, 'filter' => $filter]);
