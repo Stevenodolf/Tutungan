d6fcd7c48aeb46bb16fd015ea618064356a26274
Stevenodolf <stevenodolf@gmail.com>
2022-01-27 18:49:34 +0700

    remove probar

--- public/js/src/home/home.js
+++ public/js/src/home/home.js
@@ -42,28 +42,28 @@ $(document).ready(function(){
         },
     });
 
-    let proBar = new ProBar({
-        bgColor: "#C4C4C4",
-        color:"#DE3E16",
-        speed:0.2,
-        wrapper:".progressBar",
-        height:10,
-        classNameBar : "timer",
-        wrapperId : "progressBarId",
-        finishAnimation : false,
-        rounded : { // use it to round Corners of Probar.
-            topLeft : 2,
-            topRight : 2,
-            bottomLeft : 2,
-            bottomRight : 2
-        },
-        roundedInternal : { // use it to round Corners of Probar (internal).
-            topLeft : 2,
-            topRight : 2,
-            bottomLeft : 2,
-            bottomRight : 2
-        }
-    });
-
-    proBar.goto(30);
+    // let proBar = new ProBar({
+    //     bgColor: "#C4C4C4",
+    //     color:"#DE3E16",
+    //     speed:0.2,
+    //     wrapper:".progressBar",
+    //     height:10,
+    //     classNameBar : "timer",
+    //     wrapperId : "progressBarId",
+    //     finishAnimation : false,
+    //     rounded : { // use it to round Corners of Probar.
+    //         topLeft : 2,
+    //         topRight : 2,
+    //         bottomLeft : 2,
+    //         bottomRight : 2
+    //     },
+    //     roundedInternal : { // use it to round Corners of Probar (internal).
+    //         topLeft : 2,
+    //         topRight : 2,
+    //         bottomLeft : 2,
+    //         bottomRight : 2
+    //     }
+    // });
+    //
+    // proBar.goto(30);
 });
