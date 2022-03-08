08b4353fb0ac5ea22ac8ebfe74a75d71aa09abba
Luwis Lim <luwislim17@gmail.com>
2022-03-08 22:35:27 +0700

    Update agreement.blade.php

--- resources/views/wish/agreement.blade.php
+++ resources/views/wish/agreement.blade.php
@@ -10,10 +10,14 @@
     <main style="min-height: 100vh;">
         <div class="login">
             <div class="loginContainer">
-                <h1>Tutungan Agreement</h1>
-                <p>1. Proses verifikasi Wish akan dilakukan maksimal 1x24 jam setelah anda mengajukan pembuatan Wish.</p>
-                <p>2. Wish Anda akan ditampilkan di website ini selama 7 hari (168 jam). Apabila setelah 7 hari target kontribusi</p>
-                <p>   Wish anda tercapai, maka Wish anda akan diproses sesuai prosedur. Apabila tidak tercapai, maka Wish akan dibatalkan.</p>
+                <div class="agreement">
+                    <h1>Tutungan Agreement</h1>
+                    <p class="contentSemiNormal">1. Proses verifikasi Wish akan dilakukan maksimal 1x24 jam setelah anda mengajukan pembuatan Wish.</p>
+                    <p class="contentSemiNormal">2. Wish Anda akan ditampilkan di website ini selama 7 hari (168 jam). Apabila setelah 7 hari target kontribusi 
+                        Wish anda tercapai, maka Wish anda akan diproses sesuai prosedur. Apabila tidak tercapai, maka Wish akan dibatalkan.</p>
+                    <p class="contentSemiNormal">3. Walaupun target Wish sudah tercapai namun deadline Wish belum tercapai, User lain masih bisa menambahkan
+                         order ke dalam Wish anda dengan harga yang tetap.</p>
+                </div>
             </div>
         </div>
     </main>
